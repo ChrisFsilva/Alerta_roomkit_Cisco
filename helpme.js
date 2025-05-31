59b73ce24da5319451d5e9d7f5a6b508eee5b816
Christopher Silva <131322948+ChrisFsilva@users.noreply.github.com>
2025-05-31 11:12:12 -0300

    Versão Vtal
    Versão da API criada pensando no ambiente do cliente Vtal, onde seu ambiente power platforms é restrito impedindo o uso de links publicos, o link de comunicação da API obrigatoriamente precisa ser publico então utilizei o webhook como intermediario, criei um link nele e sempre que esse link é acionado inicia o fluxo do power automate

---
+++ helpme.js
@@ -0,0 +1,51 @@
+// Invocar biblioteca
+
+import xapi from 'xapi';
+
+ 
+
+// Endereço do webhook
+
+const webhookURL = 'https://caminho_da_URL.com'; // substitua pela sua URL do webhook
+
+ 
+
+// Ação de clique no botão
+
+xapi.Event.UserInterface.Extensions.Panel.Clicked.on(event => {
+
+  if (event.PanelId === 'helpme') {
+
+    // Dados da sala solicitante
+
+    const payload = {
+
+      room: 'Nome da Sala',
+
+      address: 'Andar/Unidade da sala',
+
+      problem: 'Mensagem de alerta'
+
+    };
+
+ 
+
+// Ação de POST no webhook
+
+    xapi.Command.HttpClient.Post({
+
+      // Variavel de atalho do webhook
+
+      Url: webhookURL,
+
+      Header: ['Content-Type: application/json'],
+
+      AllowInsecureHTTPS: true
+
+    }, JSON.stringify(payload));
+
+  }
+
+});
+
+ 
